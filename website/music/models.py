aaa9ff8a2c045769dfc25093d61684851823c5a9
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-18 15:59:39 +0530

    DB Added

--- website/music/models.py
+++ website/music/models.py
@@ -10,6 +10,8 @@ class Album(models.Model):
     genre = models.CharField(max_length=100)
     album_logo = models.CharField(max_length=1000)
     # link for logo
+
+    #STRING REPRESENTATION
     def __str__(self):
         return self.artist+'-'+self.album_title
 
@@ -18,6 +20,11 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title= models.CharField(max_length=250)
+
+    #STRING REPRESENTATION
+    def __str__(self):
+        return self.song_title
+
 
 
 
