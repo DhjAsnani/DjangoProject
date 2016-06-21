4ba6e8dc641ab3b0c383e33748e3f50733a7cf8f
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-21 13:54:01 +0530

    Updated

--- website/music/views.py
+++ website/music/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.http import Http404
 from django.template import loader
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album,Song
 
 
 def index(request):
@@ -19,7 +19,7 @@ def favourite(request, album_id):
     try:
         selected_song = album.song_set.get(pk=request.POST['song'])
          #getting value of selected song
-    except KeyError:
+    except (KeyError, Song.DoesNotExist):
         return render(request, 'music/detail.html', {'album': album, 'error_message': "No songs selected", })
     else:
         selected_song.is_favourite = True
