bf9276d5a95f19878c18e19b83dcb09ae386823a
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-21 12:57:14 +0530

    Form implemented Successfully

--- website/music/views.py
+++ website/music/views.py
@@ -14,4 +14,16 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
 
+def favourite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+         #getting value of selected song
+    except KeyError:
+        return render(request, 'music/detail.html', {'album': album, 'error_message': "No songs selected", })
+    else:
+        selected_song.is_favourite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album})
+
 
