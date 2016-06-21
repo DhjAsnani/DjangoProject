99634dd42cf38b3833177f321f78e653979a10e7
dhjasnani <asnani.dheeraj@yahoo.com>
2016-06-21 11:54:01 +0530

    Linking error removed

--- website/music/urls.py
+++ website/music/urls.py
@@ -3,8 +3,11 @@
 from django.conf.urls import url
 from . import views
 
+app_name = 'music'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+
    ]
--- website/music/views.py
+++ website/music/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.http import Http404
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 
@@ -11,9 +11,7 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-
-    except Album.DoesNotExist:
-        raise Http404("Album Not Found")
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
+
+
